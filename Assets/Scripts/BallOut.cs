763fad96bb7bc2ddc6b0801b8b68fe5e1add8e63
Chris Castelo <c_pc@hotmail.com>
2022-08-27 10:35:28 -0700

    LQ3 Gameplay Improvements
    -Adding out of bounds script BallOut.cs to improve training
    -Project settings to windowed application
    -Animation improvements (mecanim)

---
+++ Assets/Scripts/BallOut.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class BallOut : MonoBehaviour
+{
+    SpeedBallEnvController speedBallEnvController;
+    private void Start()
+    {
+        speedBallEnvController = GetComponentInParent<SpeedBallEnvController>();
+    }
+
+    void ResetScene()
+    {
+        if (speedBallEnvController.team1AgentGroup != null)
+        {
+            speedBallEnvController.team1AgentGroup.GroupEpisodeInterrupted();
+        }
+
+        if (speedBallEnvController.team2AgentGroup != null)
+        {
+            speedBallEnvController.team2AgentGroup.GroupEpisodeInterrupted();
+        }
+        Debug.Log("Out of bounds, Reseting scene");
+        
+        Invoke("ResetSceneInSeconds", 0.5f);
+    }
+
+    public void ResetSceneInSeconds()
+    {
+        speedBallEnvController.ResetScene();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            ResetScene();
+        }
+        if (col.gameObject.tag == "PlayerTeam1" || col.gameObject.tag == "OponentTeam")
+        {
+            ResetScene();
+        }
+
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            ResetScene();
+        }
+        if (col.gameObject.tag == "Team1" || col.gameObject.tag == "Team2")
+        {
+            ResetScene();
+        }
+    }
+}
